cd3db106faec41de814399965b32e7cdb19f884e
chalvare <chalvare@ucm.es>
2020-03-24 20:03:52 +0100

    Clean code and refactor

--- src/main/java/com/razbank/razbank/controllers/customer/CreateCustomerController.java
+++ src/main/java/com/razbank/razbank/controllers/customer/CreateCustomerController.java
@@ -10,46 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
-@RequestMapping("/createaccount")
+@RequestMapping("/createAccount")
 public class CreateCustomerController {
 
     private CreateCustomerService createCustomerService;
-
-    @PostMapping("/test")
-    public String addNote(@RequestBody CustomerModel customerModel, HttpServletRequest request) {
-        //get the notes from request session
-        List<CustomerModel> notes = (List<CustomerModel>) request.getSession().getAttribute("SESSION");
-        //check if notes is present in session or not
-        if (notes == null) {
-            notes = new ArrayList<>();
-            // if notes object is not present in session, set notes in the request session
-            request.getSession().setAttribute("SESSION", notes);
-        }
-        notes.add(customerModel);
-        request.getSession().setAttribute("SESSION", notes);
-
-        return null;
-    }
-    @GetMapping("/home")
-    public String home(HttpSession session) {
-        List<CustomerModel> customerModelList = (List<CustomerModel>) session.getAttribute("SESSION");
-        return null;
-    }
-    @PostMapping("/invalidate/session")
-    public String destroySession(HttpServletRequest request) {
-        //invalidate the session , this will clear the data from configured database (Mysql/redis/hazelcast)
-        request.getSession().invalidate();
-        return null;
-    }
 
     @Autowired
     public CreateCustomerController(CreateCustomerService createCustomerService) {
@@ -59,11 +28,23 @@ public class CreateCustomerController {
     @PostMapping(value="/create",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<String> create(@RequestBody CustomerModel customerModel){
+    public ResponseEntity<CustomerModel> create(@RequestBody CustomerModel customerModel,HttpServletRequest request){
         CustomerDTO customerDTO = new CustomerDTO();
         BeanUtils.copyProperties(customerModel, customerDTO);
+        request.getSession().setAttribute("SESSION", customerDTO);
+
         Response response=createCustomerService.createCustomer(customerDTO);
         if(response.getCode()!=0)throw new CreateCustomerException(Response.ERROR.getMethod()+": ERROR CREATING CUSTOMER: "+customerDTO.toString());
-        return new ResponseEntity<>( "User Created: "+response, HttpStatus.CREATED);
+        CustomerDTO c = (CustomerDTO) request.getSession().getAttribute("SESSION");
+        BeanUtils.copyProperties(c, customerModel);
+
+        return new ResponseEntity<>(customerModel, HttpStatus.CREATED);
     }
+
+    @PostMapping("/invalidate/session")
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return null;
+    }
+
 }
