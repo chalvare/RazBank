25401889538d0b85371a1effaca4b74f657bdc7b
chalvare <chalvare@ucm.es>
2020-04-17 21:47:40 +0200

    comments

--- src/main/java/com/razbank/razbank/exceptions/RazBankException.java
+++ src/main/java/com/razbank/razbank/exceptions/RazBankException.java
@@ -4,6 +4,16 @@ import com.razbank.razbank.utils.ResponseInfo;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <h1>RazBank Exception</h1>
+ * Custom exception for RazBank
+ * <p>
+ * <b>Note:</b> N/A
+ *
+ * @author Christian Álvarez
+ * @version 1.0
+ * @since 2020-04-17
+ */
 @Getter
 @Setter
 public class RazBankException extends Exception {
@@ -12,6 +22,14 @@ public class RazBankException extends Exception {
     private final String where;
     private final Object param;
 
+    /**
+     * Constructor
+     *
+     * @param message of the error
+     * @param responseInfo info about error
+     * @param where indicates where error occurs
+     * @param param additional information such as object
+     */
     public RazBankException(String message, ResponseInfo responseInfo, String where, Object param) {
         super(message);
         this.responseInfo = responseInfo;
