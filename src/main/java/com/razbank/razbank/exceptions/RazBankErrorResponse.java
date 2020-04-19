e4d5df6e038fbb0ab7a44b090e17fac63769b7e9
chalvare <chalvare@ucm.es>
2020-04-19 17:30:21 +0200

    refactor exceptions logic

--- src/main/java/com/razbank/razbank/exceptions/RazBankErrorResponse.java
+++ src/main/java/com/razbank/razbank/exceptions/RazBankErrorResponse.java
@@ -1,10 +1,20 @@
-package com.razbank.razbank.exceptions.generic;
+package com.razbank.razbank.exceptions;
 
 import com.razbank.razbank.utils.ResponseInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <h1>RazBank Exception response</h1>
+ * Custom exception for RazBank
+ * <p>
+ * <b>Note:</b> N/A
+ *
+ * @author Christian Álvarez
+ * @version 1.0
+ * @since 2020-04-19
+ */
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/com/razbank/razbank/exceptions/generic/RazBankExceptionHandler.java
+++ src/main/java/com/razbank/razbank/exceptions/generic/RazBankExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.razbank.razbank.exceptions.generic;
 
+import com.razbank.razbank.exceptions.RazBankErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
